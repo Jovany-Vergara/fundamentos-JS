248797aa947b622960e85b39f1264a00fe55f718
Jovany Vergara <jovany@makingdevs.com>
2022-08-12 12:14:07 -0500

    [main] add type of variables in js what let, const etc

---
+++ js/01.js
@@ -0,0 +1,15 @@
+// variables
+
+console.log('Hello World');
+
+//re asignar valor a una variable
+let cliente = 'Jovany';
+let edad = 25
+
+// Pueden iniciar sin valor
+let nombre;
+let apellido;
+
+
+console.log(cliente);
+console.log(edad);
