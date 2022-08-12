6c94742aa9eff90857a5042bdae127532bd4a7ba
Jovany Vergara <jovany@makingdevs.com>
2022-08-12 12:20:52 -0500

    [main] add types of variables what const in JS

---
+++ js/02.js
@@ -0,0 +1,11 @@
+const cliente = 'Jovany';
+const precioDescuento = 100;
+
+// const no se puede reasignar
+// ejemplo cliente = 'Juan';
+
+// const no se puede iniciar sin valor
+// ejemplo const nombre;
+
+console.log(cliente);
+console.log(precioDescuento);
