66430cbbc487804c840e8e3f11958e69cd97a9e1
Jovany Vergara <jovany@makingdevs.com>
2022-08-16 09:43:07 -0500

    [main] create the types of data in JS

--- js/03.js
+++ js/03.js
@@ -1,6 +1,33 @@
 // Tipos de datos
 
 // Undefined
-let cliente = 'Jovany';
-console.log(cliente);
-console.log(typeof cliente);
+//let cliente = 'Jovany';
+//console.log(cliente);
+//console.log(typeof cliente);
+
+// Bolean
+//const descuento = false;
+//console.log(descuento);
+//console.log(typeof descuento);
+
+// Number
+//const precio = 20.20;
+//const precio2 = 30;
+//const precio3 = -100;
+//console.log(typeof precio);
+//console.log(typeof precio2);
+//console.log(typeof precio3);
+
+// String o cadenas de texto
+//const nombre = 'Jovany';
+//let producto = 'Monitor';
+
+// const numero = '20';
+// const numero2 = 30;
+
+// console.log(typeof numero);
+// console.log(typeof numero2);
+
+// BigInt
+// const numero = BigInt(2079888888888987989798797787987);
+// console.log(typeof numero);
