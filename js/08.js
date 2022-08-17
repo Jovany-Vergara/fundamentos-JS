29ec2420964835b7ca2b2e60e2ea2b78e7c5b569
Jovany Vergara <jovany@makingdevs.com>
2022-08-17 14:44:29 -0500

    [main] introducction in arrays in JS

---
+++ js/08.js
@@ -0,0 +1,9 @@
+// Arrays o arreglos
+
+const tecnologias = [20, 'JavaScript', 'ReactJS', 'React Native', true];
+
+console.log(tecnologias[2]);
+console.table(tecnologias)
+
+console.log(tecnologias.length);
+console.log(tecnologias.toString());
