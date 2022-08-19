a4cca1e34a296b7eac069142eb1e636444882fd5
Jovany Vergara <jovany@makingdevs.com>
2022-08-19 11:53:48 -0500

    [main] iterators of arrays

---
+++ js/11.js
@@ -0,0 +1,14 @@
+// Iteradores en Javascript
+const tecnologias = ['HTML', 'CSS', 'JavaScript', 'React', 'Redux', 'NodeJS'];
+
+// fotEach - accede a cada elemento del arreglo
+const arrayForeach = tecnologias.forEach(function( teach){
+  return teach
+})
+
+const arrayMAp = tecnologias.map(function(teach){
+  return teach
+});
+
+console.log(arrayForeach);
+console.log(arrayMAp);
