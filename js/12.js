91295439e7db5b991b479fc86315cc03b996dc21
Jovany Vergara <jovany@makingdevs.com>
2022-08-30 16:57:48 -0500

    [main] return fuctions

--- js/12.js
+++ js/12.js
@@ -1,9 +1,20 @@
 // funciones - function declaration
 
-function sumar(num1, num2 = 0) {
-    console.log(num1 + num2)
+//function sumar(num1, num2 = 0) {
+//    console.log(num1 + num2)
+//}
+
+//sumar(10,2)
+//sumar(8,2)
+//sumar(100)
+
+function sumar(num1 = 0, num2 = 0) {
+    return {
+        resultado: num1 = num2,
+        mensaje: 'hola'
+    }
 }
 
-sumar(10,2)
-sumar(8,2)
-sumar(100)
+const {resultado, mensaje} = sumar(10,2)
+console.log(resultado)
+console.log(mensaje)
