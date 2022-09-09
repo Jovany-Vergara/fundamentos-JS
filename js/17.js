4ecb01e68ef3be22544fa054f7365522994141e5
Jovany Vergara <jovany@makingdevs.com>
2022-09-09 13:24:32 -0500

    [main] create conditionals with if in JS

---
+++ js/17.js
@@ -0,0 +1,12 @@
+// condicionales
+
+const disponible = 400;
+const retirar = 2002;
+
+if(disponible > retirar){
+  // se cumple la condición y se ejecuta el código
+  console.log("Retirando efectivo")
+} else{
+  // no se cumple la condición
+  console.log("Fondos insuficientes")
+}
